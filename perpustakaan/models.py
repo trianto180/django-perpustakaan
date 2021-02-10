51124b92bfcf724679164d47177076f7c8a634c2
Angga <angga.trianto@students.amikom.ac.id>
2021-02-10 21:36:30 +0700

    models

--- perpustakaan/models.py
+++ perpustakaan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Buku(models.Model):
+    judul = models.CharField(max_length=50)
+    penulis = models.CharField(max_length=40)
+    penerbit = models.CharField(max_length=40)
+    jumlah = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.judul
