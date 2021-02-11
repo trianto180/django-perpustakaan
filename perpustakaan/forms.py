a84e1eb24f299327d4b5fd36ff79799e8f44e3e9
Angga <angga.trianto@students.amikom.ac.id>
2021-02-11 20:26:15 +0700

    forms widget bootstrap

--- perpustakaan/forms.py
+++ perpustakaan/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from perpustakaan.models import Buku
 
 class FormBuku(ModelForm):
@@ -6,3 +7,11 @@ class FormBuku(ModelForm):
         model = Buku
         # fields = ['judul', 'penulis', 'kelompok_id']
         exclude = ['penerbit']
+
+        widgets = {
+            'judul' : forms.TextInput({'class':'form-control'}),
+            'penulis' : forms.TextInput({'class':'form-control'}),
+            'penerbit' : forms.TextInput({'class':'form-control'}),
+            'jumlah' : forms.NumberInput({'class':'form-control'}),
+            'kelompok_id' : forms.Select({'class':'form-control'}),
+        }
