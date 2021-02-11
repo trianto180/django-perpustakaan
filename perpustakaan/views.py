5246315b208221c8d790a62220aa5af4b8e7b4c3
Angga <angga.trianto@students.amikom.ac.id>
2021-02-11 22:34:43 +0700

    update data

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perpustakaan.models import Buku
 from perpustakaan.forms import FormBuku
+from django.contrib import messages
+
+def ubah_buku(request, id_buku):
+    buku = Buku.objects.get(id=id_buku)
+    template = 'ubah-buku.html'
+    if request.POST:
+        form = FormBuku(request.POST, instance=buku)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Data Berhasil diperbarui!")
+            return redirect('ubah_buku', id_buku=id_buku)
+    else:
+        form = FormBuku(instance=buku)
+        konteks = {
+            'form' : form,
+            'buku' : buku,
+        }
+    return render(request, template, konteks)
 
 def buku(request):
-    # books = Buku.objects.all()
+    books = Buku.objects.all()
     #menampilkan buku jumla 32
-    books = Buku.objects.filter(jumlah=32)
+    # books = Buku.objects.filter(jumlah=32)
 
     #menampilkan buku sesuai kategori, [:1/2] untuk melimit jumlah yang ditampilkan
-    books = Buku.objects.filter(kelompok_id__nama='produktif')[:1]
+    # books = Buku.objects.filter(kelompok_id__nama='produktif')[:1]
 
     konteks = {
         'books': books,
